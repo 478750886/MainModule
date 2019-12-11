280d4336ad5d59e6d3735c094f6c2151102ca136
xiakun <xiakun@cjtc.net.cn>
2019-12-11 17:02:04 +0800

    创建消费中心

---
+++ eurekaClient/src/main/java/com/cjtc/eurekaclient/EurekaclientApplication.java
@@ -0,0 +1,18 @@
+package com.cjtc.eurekaclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@Slf4j
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaclientApplication.class, args);
+        log.info("消费中心启动成功");
+    }
+
+}
